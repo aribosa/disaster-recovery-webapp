50ac379c3f9cd691521e203026d37c1b7da41d51
aribosa <arielbosano@gmail.com>
2022-06-22 23:17:54 -0300

    Initial Commit

---
+++ disaster_response_pipeline_project/data/process_data.py
@@ -0,0 +1,79 @@
+
+import sys
+import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+import pandas as pd
+
+
+def load_data(messages_filepath, categories_filepath):
+    disaster_categories = pd.read_csv('disaster_categories.csv')
+    disaster_messages = pd.read_csv('disaster_messages.csv')
+
+    return pd.merge(disaster_messages, disaster_categories, on='id')
+
+
+def clean_data(df):
+
+    df_categories = df.categories.str.split(';', expand=True)
+    column_names = [x.split('-')[0] for x in df_categories.iloc[0]]
+    df_categories.columns = column_names
+
+    for col in df_categories.columns:
+        df_categories[col] = df_categories[col].str.split('-').str.get(1)
+        df_categories[col] = df_categories[col].astype(int)
+    df_categories['id'] = df['id']
+    df = pd.concat([df, df_categories], axis=1).drop('categories', axis=1).iloc[:, :-1].drop_duplicates()
+
+    for genre in df.genre.unique():
+        df[f'is_{genre}'] = (df['genre'] == genre).astype(int)
+
+    df.drop('genre', axis=1, inplace=True)
+
+    return df
+
+
+def save_data(df, database_filename):
+
+    connection = create_engine(f'sqlite:///{database_filename}')
+    try:
+        connection.execute('SELECT 1 FROM database_messages')
+        print('Table already exists, deleting files...')
+
+    except OperationalError:
+        pass
+
+    df.to_sql('database_messages', connection, if_exists='replace', index=False)
+    print('Table Created/Updated')
+
+    return
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -0,0 +1,209 @@
+"""
+Classification Model
+Full ML Pipeline to detect disaster messages among social media, text or web articles.
+
+Usage:
+    Python Script
+    > python3 train_classifier.py <path to sqllite db file> <path to the models pickle file>
+
+    Python Script Sample
+    > python3 train_classifier.py DisasterResponse.db ./model/classif_model.pkl
+
+Args:
+    db_file_source: directory for the Database model (.db file, containing sqlite schema and data)
+    model_pickle: pickle object with pre-trained model
+"""
+
+# NLP libraries
+import nltk
+# nltk.download('punkt')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+
+# Data
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+import sys
+import datetime
+import os
+import re
+import pickle
+
+# ML models
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+
+
+def load_data(database_filepath, n_rows=10):
+    """
+    Returns both target and feature vectors from the messages database
+
+    Args:
+        database_filepath::str
+            Database filepath to retrieve the data using SQLite
+
+    Returns:
+        X::pd.Series
+            Pandas series containing the feature vector
+
+        y::pd.DataFrame
+            Dataframe with all the target values for all the observations
+
+        categories::[str]
+            List with all category names from the given dataset
+
+    """
+    engine = create_engine('sqlite:///'  + database_filepath)
+    data_frame = pd.read_sql(f'SELECT * FROM database_messages LIMIT {n_rows}', engine)
+
+    return data_frame['message'], data_frame.iloc[:, 3:]
+
+
+def tokenize(text):
+    """
+    Performs a tokenization to the provided text to that feature texts can be seen as vectors
+    to our Machine Learning pipeline.
+
+    Args:
+        text:str
+            message string to split
+
+    Returns
+        vector:[str]
+            array with tokens
+    """
+
+    # Replace url hyperlinks with special place holders
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    urls = re.findall(url_regex, text)
+
+    # Crawl for each found URL and replace in the original text
+    for url in urls:
+        text = text.replace(url, 'url_placeholder')
+
+    # Divide sentences into words (splitted by space)
+    tokens = nltk.word_tokenize(text)
+
+    # Lemmatize all the tokens to obtain the word's stem or root representation
+    lem = nltk.stem.WordNetLemmatizer()
+    clean_tokens = [lem.lemmatize(token).lower().strip() for token in tokens]
+    clean_tokens = list(filter(lambda x: len(x) > 2, clean_tokens))  # Filter tokens with less than 2 characters
+    clean_tokens = [token for token in clean_tokens if token in stopwords.words('english')]
+    return clean_tokens
+
+
+class LoggerTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X:pd.DataFrame, y=None):
+        print(f'Starting Model traning with an array of shape {X.shape}')
+        return X
+
+
+class StartingVerbTransformer(BaseEstimator, TransformerMixin):
+
+    @staticmethod
+    def start_verb(text):
+        try:
+            sentences = sent_tokenize(text)
+            for sentence in sentences:
+                pos_tags = nltk.pos_tag(tokenize(sentence))
+                f_word, f_tag = pos_tags[0]
+                if f_tag in ['VB', 'VBP'] or f_word == 'RT':
+                    return True
+        except:
+            return False
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_new = pd.DataFrame(pd.Series(X).apply(self.start_verb))
+        return X_new
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('pipeline-nlp', Pipeline([
+                ('vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf-transformer', TfidfTransformer())
+            ])),
+
+            ('sentence-tagger', StartingVerbTransformer())
+        ])),
+        ('logging', LoggerTransformer()),
+
+        ('classification', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names=None):
+    Y_prediction = model.predict(X_test)
+    Y_pred = pd.DataFrame(Y_prediction, columns=Y_test.columns)
+
+    # conf_matrix = confusion_matrix(Y_test, Y_prediction)
+    # print('Confusion Matrix (Actual Values / Predicted')
+    # print(conf_matrix)
+
+
+    for i, column in enumerate(Y_test.columns):
+        print(f'Model performance over feature {column}: {accuracy_score(Y_test[column], Y_pred[column])}')
+
+    return
+
+
+def save_model(model, model_filepath):
+    pass
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+
+        for n in range(100, 50000, 10000):
+            start = datetime.datetime.now()
+            X, Y = load_data(database_filepath, n_rows=n)
+            X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+            print('Building model...')
+            model = build_model()
+
+            print('Training model...')
+            model.fit(X_train, Y_train)
+
+            print('Evaluating model...')
+            evaluate_model(model, X_test, Y_test)
+
+            print('Saving model...\n    MODEL: {}'.format(model_filepath))
+            # save_model(model, model_filepath)
+
+            print('Trained model saved!')
+            print(f'Total time using {n} rows: {datetime.datetime.now() - start}')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
